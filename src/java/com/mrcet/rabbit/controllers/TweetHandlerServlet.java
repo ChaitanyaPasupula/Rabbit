ebef27c0993666fc16738ae6da49e41b0dd0eccd
ChaitanyaPasupula <31741539+ChaitanyaPasupula@users.noreply.github.com>
2017-09-07 12:36:27 -0500

    Add files via upload

---
+++ src/java/com/mrcet/rabbit/controllers/TweetHandlerServlet.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mrcet.rabbit.controllers;
+
+import com.mrcet.rabbit.Beans.Tweet;
+import com.mrcet.rabbit.Beans.User;
+import com.mrcet.rabbit.services.TweetService;
+import com.mrcet.rabbit.services.impl.TweetServiceImpl;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author hp
+ */
+public class TweetHandlerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            HttpSession httpSession = request.getSession(false);
+            if(httpSession == null) response.sendRedirect("./jsp/error.jsp");
+            String tweetS = request.getParameter("tweet");
+            String username = (String)httpSession.getAttribute("username");
+            User user = new User();
+            user.setEmail(username);
+            Tweet tweet = new Tweet(tweetS,user);
+            TweetService tweetService = new TweetServiceImpl();
+            boolean status = tweetService.addTweet(tweet);
+            if(status) request.getRequestDispatcher("./generatedash").forward(request, response);
+            else response.sendRedirect("/logout");
+        }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
